92a3861a8d09fd797978e6436790d1a81459fe88
Stefan Ziegler <edi.gonzales@gmail.com>
2025-07-23 19:12:17 +0200

    test reloading

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```
 java -jar jedit.jar -log=3
 ```
+
+```
+./gradlew clean build && cp build/libs/InterlisPlugin.jar ~/Library/jEdit/jars
+```
--- src/main/java/ch/so/agi/jedit/InterlisPlugin.java
+++ src/main/java/ch/so/agi/jedit/InterlisPlugin.java
@@ -6,15 +6,13 @@ import org.gjt.sp.util.Log;
 public class InterlisPlugin extends EditPlugin {
     @Override
     public void start() {
-        System.err.println("[InterlisPlugin] wurde geladen.");
         System.err.println("[InterlisPlugin] foo.");
-        Log.log(Log.MESSAGE, this, "InterlisPlugin started");
+        Log.log(Log.MESSAGE, this, "[InterlisPlugin] started");
     }
 
     @Override
     public void stop() {
-        System.err.println("[InterlisPlugin] wurde entladen.");
         System.err.println("[InterlisPlugin] bar.");
-        Log.log(Log.MESSAGE, this, "InterlisPlugin stopped");
+        Log.log(Log.MESSAGE, this, "[InterlisPlugin] stopped");
     }
 }
